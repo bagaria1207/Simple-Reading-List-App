2184a28bc9d0df5627cbef38144cc777c8ebdf00
Apurva <bagaria.apurva@gmail.com>
2020-06-15 09:22:53 +0530

    Delete Event with Event Bubbling

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // Delete Books From List
-var btns = document.querySelectorAll('#book-list .delete');
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click', function(e){
+// var btns = document.querySelectorAll('#book-list .delete');
+// Array.from(btns).forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//         const li = e.target.parentElement;
+//         li.parentElement.removeChild(li);
+//     });
+// });
+
+
+// Delete Event With Event Bubbling
+const list = document.querySelector('#book-list ul');
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
         const li = e.target.parentElement;
-        li.parentElement.removeChild(li);
-    });
+        // li.parentNode.removeChild(li);
+        list.removeChild(li);
+    }
 });
